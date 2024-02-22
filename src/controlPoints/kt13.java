8b46c74e27b03e737ffe7c7a393781634717cab7
BaBaBaXXX <rhbpbc321@gmail.com>
2024-02-22 19:20:04 +0500

    ))

--- src/controlPoints/kt13.java
+++ src/controlPoints/kt13.java
@@ -1,16 +1,24 @@
+package controlPoints;
+// делал для 14 Задания Leetcode, должно работать на любой элемент любого массива
 public class kt13 {
     public static void main(String[] args) {
-        String[] str = new String[3];
-        str[0] = "flower";
-        str[1] = "flow";
-        str[2] = "flight";
-        StringBuilder strBuild = new StringBuilder();
-        for (int i = 0; i < lengthCalc(str) ; i++) {
-            if (str[0].charAt(i) == str[1].charAt(i) && str[0].charAt(i) == str[2].charAt(i)) {
-                strBuild.append(str[0].charAt(i));
+        String[] strs = new String[]{"flower", "flow", "flower"};
+        StringBuilder str = new StringBuilder();
+        for (int j = 0; j < lengthCalc(strs); j++) {
+            boolean biba = false;
+            for (int i = 0; i < strs.length; i++) {
+                if (strs[0].charAt(j) == strs[i].charAt(j)) {
+                    biba = true;
+                }
+                else {
+                    System.out.println(str);
+                    System.exit(1);
+                }
             }
+            if (biba)
+                str.append(strs[0].charAt(j));
         }
-        System.out.println(strBuild);
+        System.out.println(str);
     }
 
     public static int lengthCalc (String[] strs) {
@@ -19,4 +27,15 @@ public class kt13 {
             min = Math.min(strs[i].length(), min);
         return min;
     }
+
+    /*public static void compareToBuild (StringBuilder str, String[] strs) {
+        StringBuilder st = new StringBuilder();
+        for (int j = 0; j < lengthCalc(strs); j++) {
+            for (int i = 0; i < strs.length; i++) {
+                if (strs[0].charAt(j) == strs[i].charAt(j)) {
+                    st.append(strs[0].charAt(j));
+                }
+            }
+        }
+    }*/
 }
